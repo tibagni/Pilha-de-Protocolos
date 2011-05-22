93b95ce19dc15d530bec2e4c53ade0c8f6131586
Tiago Bagni <tibagni@gmail.com>
2011-05-22 14:47:12 -0300

    Transport layer classes

---
+++ pilha_protocolos/src/pdu/Segment.java
@@ -0,0 +1,14 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package pdu;
+
+/**
+ *
+ * @author tiago
+ */
+public class Segment {
+
+}
---
+++ pilha_protocolos/src/pilha_protocolos/MySocket.java
@@ -0,0 +1,15 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package pilha_protocolos;
+
+/**
+ * Substitui a classe Socket para utilizar a pilha de protocolos
+ * do projeto.
+ * @author tiago
+ */
+public class MySocket {
+
+}
