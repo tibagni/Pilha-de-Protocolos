f9b7f011ee8eefdab0f97c554a654a547f1ec292
iscaro <cabelitos@uol.com.br>
2011-05-22 15:30:07 -0300

    segment

--- pilha_protocolos/src/pdu/Segment.java
+++ pilha_protocolos/src/pdu/Segment.java
@@ -11,4 +11,38 @@ package pdu;
  */
 public class Segment {
 
+    private int sourcePort;
+    private int destPort;
+    private int seqNumber;
+    private int ackNum;
+    private byte[] data;
+    private int windowSize;
+    private int protocol;
+
+    public Segment(){
+
+    }
+    
+    public int getProtocol(){
+        return protocol;
+    }
+    public int getWindowSize(){
+        return windowSize;
+    }
+    public int getSourcePort(){
+        return sourcePort;
+    }
+    public int getDestPort(){
+        return destPort;
+    }
+    public int getSeqNumber(){
+        return seqNumber;
+    }
+    public int getAck(){
+        return ackNum;
+    }
+    public byte[] getData(){
+        return data;
+    }
+
 }
